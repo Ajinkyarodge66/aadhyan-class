1bb1297da07e84794d012d5480c2dd5362d80c9e
Ajinkyarodge66 <79162398+Ajinkyarodge66@users.noreply.github.com>
2025-12-11 19:11:02 +0530

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// IMPORTANT: Only ONE export default allowed
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/aadhyan-class/",  // your repo name
+});
+
